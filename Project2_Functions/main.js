a7a9a5ec3142d230ae2772fd58f8a0b0ae83e59d
madisailors <madisailors@yahoo.com>
2022-08-06 10:30:41 -0700

    functions submission

---
+++ Project2_Functions/main.js
@@ -0,0 +1,10 @@
+function button() {
+    var str = "so glad it worked";
+    document.getElementById("Button_Text").innerHTML= str;
+}
+
+function function1() {
+    var sentence= "I am ";
+    sentence += " having a blast";
+    document.getElementById("concatenate").innerHTML = sentence;
+}
