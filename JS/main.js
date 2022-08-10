5e8ee4abed26369b4618da3056f3b013232aad3e
madisailors <madisailors@yahoo.com>
2022-08-10 14:28:15 -0700

    random edits

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,21 @@
+function addition() {
+    var addition= 7 + 2;
+    document.getElementById("Add").innerHTML="7 + 2 = " + addition;
+}
 
-
-function my_first_function() {  //Defining a function and naming it
-    var str= "This is the button text!";  //defining a variable and giving it 
-                                        //a string value
-    document.getElementsById("Button_Text").innerHTML= str; //putting value of the variable
-                                        //into the HTML elementFromPoint
-                                        //w the button_text id
+function subtraction() {
+    var subtraction= 7 - 2;
+    document.getElementById("Subtract").innerHTML="7 - 2 = " + subtraction;
 }
 
 
+function multiplication() {
+    var multiplication= 7 * 2;
+    document.getElementById("Multiply").innerHTML="7 * 2 = " + multiplication;
+}
 
-var B = "This is the beginning of the string"
-var C = " and this is the end of the string"
-document.write(B+C);
+
+function division() {
+    var division= 7 / 2;
+    document.getElementById("Divide").innerHTML="7 - 2 = " + division;
+}
---
+++ Project2_Functions/concatenate.js
@@ -0,0 +1,12 @@
+var B = "This is the beginning of the string"
+var C = " and this is the end of the string"
+document.write(B+C);
+
+
+function my_first_function() {  //Defining a function and naming it
+    var str= "This is the button text!";  //defining a variable and giving it 
+                                        //a string value
+    document.getElementsById("Button_Text").innerHTML= str; //putting value of the variable
+                                        //into the HTML elementFromPoint
+                                        //w the button_text id
+}
