df423d5b9d0dc6e99d6a4ef47e42b2b02fa37b1f
madisailors <madisailors@yahoo.com>
2022-08-11 11:50:45 -0700

    project 7 submission

--- project7_scope/main.js
+++ project7_scope/main.js
@@ -21,3 +21,35 @@ function addnumbers2(){               // it can display result
 
 addnumbers1();
 (addnumbers2);
+
+function getdate() {
+    if (new Date().getHours() < 18) {
+    document.getElementById("date").innerHTML = "How are you today?";
+    }
+}
+
+function age_f() {
+    age = document.getElementById("age").value;
+    if (age >= 25) {
+        ans= "You can rent a car!";
+    }
+    else {
+        ans= "You are not old enough to rent a car.";
+    }
+    document.getElementById("how_old").innerHTML = ans;
+}
+
+function time_f() {
+    var Time= new Date().getHours();
+    var reply;
+    if (Time < 12 == Time > 0) {
+        reply= "It is morning time";
+    }
+    else if (Time >= 12 == Time < 18) {
+        reply= "It is afternoon time";
+    }
+    else {
+        reply= "It is evening time.";
+    }
+    document.getElementById("time").innerHTML = reply;
+}
