5eae2c62636f36354c909dc89d1fdeb78d115987
madisailors <madisailors@yahoo.com>
2022-08-23 08:44:54 -0700

    part 7

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -23,6 +23,61 @@ function placeXOrO(squareNumber) {
             activePlayer = 'X';
         }
 
+        //this function parses the selectedSquares array to search for win conditions
+        //drawLine() function is called to draw a line on the screen if the condition is met
+        function checkWinConditions() {
+            // X 0,1,2 condition
+            if (arrayIncludes('0X','1X','2X')) {drawLine(50, 100, 558, 100)}
+            // X 3,4,5 condition
+            else if (arrayIncludes('3X','4X','5X')) {drawLine(50, 304, 558, 304)}
+            // X 6,7,8
+            else if (arrayIncludes('6X','7X','8X')) {drawLine(50, 508, 558, 508)}
+            //X 0,3,6 
+            else if (arrayIncludes('0X','3X','6X')) {drawLine(100, 50, 100, 558)}
+            //X 1,4,7
+            else if (arrayIncludes('1X','4X','7X')) {drawLine(304, 50, 304, 558)}
+            //X 2,5,8
+            else if (arrayIncludes('2X','5X','8X')) {drawLine(508, 50, 508, 558)}
+            //X 6,4,2
+            else if (arrayIncludes('6X','4X','2X')) {drawLine(100, 508, 510, 90)}
+            //X 0,4,8
+            else if (arrayIncludes('0X','4X','8X')) {drawLine(100, 100, 520, 520)}
+            //O 0,1,2
+            else if (arrayIncludes('0O','1O','2O')) {drawLine(50, 100, 558, 100)}
+            //O 3,4,5
+            else if (arrayIncludes('3O','4O','5O')) {drawLine(50, 304, 558, 304)}
+            //O 6,7,8
+            else if (arrayIncludes('6O','7O','8O')) {drawLine(50, 508, 558, 508)}
+            //O 0,3,6
+            else if (arrayIncludes('0O','3O','6O')) {drawLine(100, 50, 100, 558)}
+            //O 1,4,7
+            else if (arrayIncludes('1O','4O','7O')) {drawLine(304, 50, 304, 558)}
+            //O 2,5,8
+            else if (arrayIncludes('2O','5O','8O')) {drawLine(508, 50, 508, 558)}
+            // O 6,4,2
+            else if (arrayIncludes('6O','4O','2O')) {drawLine(100, 508, 510, 90)}
+            // O 0,4,8
+            else if (arrayIncludes('0O','4O','8O')) {drawLine(100, 100, 520, 520)}
+            //This condition checks for a tie 
+            else if (selectedSquares.length >= 9) {
+                //plays tie sound
+                Audio('./media/tie.mp3');
+                //this sets a .3 sec timer before resetGame is called
+                setTimeout(function () {resetGame();}, 500);
+            }
+
+            //checks if any array includes 3 strings
+            function arrayIncludes(squareA, squareB, squareC) {
+                //these variables will be used to check for 3 ina row
+                const a = selectedSquares.includes(squareA);
+                const b = selectedSquares.includes(squareB);
+                const c= selectedSquares.includes(squareC);
+                //if the 3 variables we pass are all included in our array
+                //then true is returned and our else if condition executes drawLine()
+                if (a === true && b === true && c === true) {return true;}
+            }
+                }
+
         //This function plays placement sound
         Audio('./media/place.mp3');
         //this condition checks to see if it's the comps turn
@@ -52,5 +107,81 @@ function placeXOrO(squareNumber) {
             };
         }
     }
+
+    //this function makes our body element temporarily unclickable
+    function disableClick() {
+        //makes body unclickable
+        body.style.pointerEvents= 'none';
+        //makes clickable after 1 second
+        setTimeout(function () { body.style.pointerEvents= 'auto';}, 1000);
+    }
+
+    function audio(audioURL) {
+        let audio = new Audio(audioURL);
+        audio.play();
+    }
     
+    //this function utilizes html canvas to draw win lines
+    function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
+        const canvas = document.getElementById('win-lines');
+        const c = canvas.getContext('2d'); //gives access to methods and properties to use on canvas
+        //indicates where the start of a lines X axis is
+        let x1= coordX1,
+            y1= coordY1,
+            x2= coordX2,
+            y2= coordY2,
+            x = x1,
+            y = y1;
+    }
+
+    //this function interacts with the canvas
+    function animateLineDrawing() {
+        //this variable creates a loop
+        const animationLoop = requestAnimationFrame(animateLineDrawing);
+        //this method clears content from last loop iteration
+        c.clearRect(0,0,608,608);
+        //starts a new path
+        c.beginPath();
+        //moves to starting point in our line
+        c.moveTo(x1, y1);
+        //indicates end point in line
+        c.lineTo(x,y);
+        //set swidth of our line
+        c.lineWidth = 10;
+        //color
+        c.strokeStyle = 'rgba(70,255,33,.8)';
+        //draws out everything we laid out above
+        c.stroke();
+        //checks if we've reached endpoints
+        if(x1 <= x2 && y1<= y2) {
+            //adds 10 to previous end x point
+            if (x < x2 ) {x += 10; }
+            //adds 10 to previous end y point
+            if (y < y2) {y += 10; }
+            //condition necesarry for 2,4,6 win conditions
+            if (x >= x2 && y >= y2) {cancelAnimationFrame(animationLoop);}
+
+        }
+        if (x1 <= x2 && y1 >= y2) {
+            if (x < x2) {x += 10;}
+            if (y > y2) {y -= 10;}
+            if (x >= x2 && y <= y2) {cancelAnimationFrame(animationLoop); }
+        }
+    }
+    // this function clears our canvas after our win line is drawn
+    function clear() {
+        //starts our animation loop
+        const animationLoop = requestAnimationFrame(clear);
+        c.clearRect(0,0,608,608);
+        //stops ainmation loop
+        cancelAnimationFrame(animationLoop);
+    }
+    //disallows clicking while win sound is playing
+    disableClick();
+    //plays win sound
+    Audio('./media/winGame.mp3');
+    //calls main animation loop
+    animateLineDrawing();
+    //waits 1 seconds, then clears canvas and resets game
+    setTimeout(function () {clear(); resetGame();}, 1000);
 }
