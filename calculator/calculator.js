1035933a0d9e640d48867201a20fe64e04c632fc
madisailors <madisailors@yahoo.com>
2022-08-23 14:48:49 -0700

    Update calculator.js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -90,7 +90,7 @@ function Update_Display() {
 }
 
 Update_Display();
-//moniters button clicks
+//monitors button clicks
 const keys = document.querySelector('.calculator-keys');
 keys.addEventListener('click', (event) => {
     //the target variable is an object that reps the 
@@ -103,7 +103,7 @@ keys.addEventListener('click', (event) => {
     if (target.classList.contains('operator')) {
         Handle_Operator(target.value);
         Update_Display();
-        return
+        return;
     }
     if (target.classList.contains('decimal')) {
         Input_Decimal(target.value);
